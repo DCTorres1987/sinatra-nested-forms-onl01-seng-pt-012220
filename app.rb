2e95fb1dc1feadf8ec4d4a30038e0ced859ef01f
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-29 23:32:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,14 @@ module FormsLab
     end
 
     post '/pirates' do
-      @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      end
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      @ships = Ship.all
+      
       erb :'pirates/show'
     end
 
